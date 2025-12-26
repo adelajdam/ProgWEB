a5d1622a4b204bfaf736b08b9077026825194eb4
Ami-1111 <Bajame.Hoxha@fti.edu.al>
2025-12-26 17:16:28 +0100

    Validimi i login dhe signup+butoni remeber me

--- Login/script.js
+++ Login/script.js
@@ -9,3 +9,58 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+// ---------- SIGN UP VALIDATION ----------
+document.getElementById("signupForm").addEventListener("submit", function(e){
+    let valid = true;
+
+    let name = signupName.value.trim();
+    let email = signupEmail.value.trim();
+    let password = signupPassword.value.trim();
+
+    nameError.textContent = "";
+    emailError.textContent = "";
+    passwordError.textContent = "";
+
+    if(name === ""){
+        nameError.textContent = "Name cannot be empty";
+        valid = false;
+    }
+
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(!email.match(emailPattern)){
+        emailError.textContent = "Enter a valid email";
+        valid = false;
+    }
+
+    if(password.length < 6){
+        passwordError.textContent = "Password must be at least 6 characters";
+        valid = false;
+    }
+
+    if(!valid) e.preventDefault();
+});
+
+
+// ---------- LOGIN VALIDATION ----------
+document.getElementById("loginForm").addEventListener("submit", function(e){
+    let valid = true;
+
+    loginEmailError.textContent = "";
+    loginPasswordError.textContent = "";
+
+    let email = loginEmail.value.trim();
+    let password = loginPassword.value.trim();
+
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(!email.match(emailPattern)){
+        loginEmailError.textContent = "Enter a valid email";
+        valid = false;
+    }
+
+    if(password === ""){
+        loginPasswordError.textContent = "Password cannot be empty";
+        valid = false;
+    }
+
+    if(!valid) e.preventDefault();
+});
